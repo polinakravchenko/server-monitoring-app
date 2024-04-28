e9588236529adff9303e37eecc56fc00199320e3
Polina Kravchenko <50082367+polinakravchenko@users.noreply.github.com>
2024-04-28 17:56:41 +0300

    Update urls.py

--- server_monitoring/servers/urls.py
+++ server_monitoring/servers/urls.py
@@ -1,6 +1,10 @@
+from rest_framework.routers import DefaultRouter
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 from . import views
+from .views import ServerViewSet
+
+server_list = ServerViewSet.as_view({'get': 'get_servers_by_localization'})
 
 urlpatterns = [
     path('servers/', views.servers, name='servers'),
@@ -8,4 +12,12 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('secure/', views.secure_page, name='secure_page'),
+    path('upload/', views.upload_form, name='upload_form'),
+    path('upload_csv/', ServerViewSet.as_view({'post': 'upload_csv'}), name='upload_csv'),
+    path('servers/get_servers_by_localization/<str:localization>/', server_list, name='get_servers_by_localization'),
 ]
+
+router = DefaultRouter()
+router.register(r'servers', ServerViewSet)
+
+urlpatterns += router.urls
